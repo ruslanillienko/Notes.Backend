9ec8c8362d221417c551fb5e987b700042822680
Ruslan Illienko <ruslan.illienko@gmail.com>
2023-11-09 02:34:01 +0200

    Clean Architecture [Edited]

---
+++ Notes.Domain/ToDoItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoLists.Domain
+{
+    public class ToDoItem
+    {
+        public Guid UserId { get; set; }
+
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "ItemName is required")]
+        public string ItemName { get; set; }
+
+        [Required(ErrorMessage = "ItemDescription is required")]
+        public string ItemDescription { get; set; }
+
+        [Required(ErrorMessage = "ItemStatus is required")]
+        public bool ItemStatus { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime? EditDate { get; set; }
+
+
+    }
+}
