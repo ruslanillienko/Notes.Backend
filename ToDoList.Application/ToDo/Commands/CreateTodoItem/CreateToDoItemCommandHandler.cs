06f64cf48a8369ad12e84c15d6ed32cd3b30cf89
Ruslan Illienko <ruslan.illienko@gmail.com>
2023-11-10 23:38:33 +0200

    CQRS, MediatR (Created first Command)

---
+++ ToDoList.Application/ToDo/Commands/CreateTodoItem/CreateToDoItemCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using ToDo.Application.Interfaces;
+using ToDo.Domain;
+
+namespace ToDo.Application.ToDo.Commands.CreateTodoItem
+{
+    internal class CreateToDoItemCommandHandler
+        : IRequestHandler<CreateToDoItemCommand, Guid>
+    {
+        private readonly IToDoDbContext _dbContext;
+
+        public CreateToDoItemCommandHandler(IToDoDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Guid> Handle(CreateToDoItemCommand request,
+            CancellationToken cancellationToken)
+        {
+            var note = new ToDoItem
+            {
+                UserId = request.UserId,
+                ItemName = request.ItemName,
+                ItemDescription = request.ItemDescription,
+                ItemStatus = false,
+                Id = Guid.NewGuid(),
+                CreationDate = DateTime.Now,
+                EditDate = null
+            };
+
+            await _dbContext.ToDoItems.AddAsync(note, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return note.Id;
+        }
+    }
+}
---
+++ ToDoList.Application/ToDo/Commands/CreateTodoItem/CreateTodoItemCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MediatR;
+
+namespace ToDo.Application.ToDo.Commands.CreateTodoItem
+{
+    internal class CreateToDoItemCommand : IRequest<Guid>
+    {
+        public Guid UserId { get; set; }
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+    }
+}
